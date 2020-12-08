9acbb499f90b6e926e6b1be550fe900b68151362
Aleksey Levykin <a.a.levykin@gmail.com>
2020-12-08 19:40:39 +0500

    Final 2020

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBIcon, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBIcon, MDBView } from "mdbreact";
 import "./index.css";
 import photo from "./photo.png";
 import logo from "./logo.svg";
+import chel from "./chel.pdf";
+import chel_diplom from "./chel_diplom.pdf";
+import chel_photo from "./chel_photo.jpg";
+import kazan from "./kazan.pdf";
+import kazan_diplom from "./kazan_diplom.pdf";
+import kazan_photo from "./kazan_photo.jpg";
+import online from "./online.pdf";
+import online_diplom from "./online_diplom.pdf";
+import ekb from "./ekb.pdf";
+import ekb_diplom from "./ekb_diplom.pdf";
+import final2020 from "./final2020.pdf";
+import final2020_diplom from "./final2020_diplom.pdf";
+import final2020_photo from "./final2020_photo.jpg";
 
 class App extends Component {
   render() {
@@ -95,10 +108,10 @@ class App extends Component {
             </MDBRow>
             <MDBRow className="mb-3">
               <MDBCol size="2">
-                <i class="fab fa-github fa-2x white-text"></i>
+                <i className="fab fa-github fa-2x white-text"></i>
               </MDBCol>
               <MDBCol size="10">
-                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href="https://github.com/AlLevykin" target="_blank">github.com/AlLevykin</a></h5>
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href="https://github.com/AlLevykin" rel="noopener noreferrer" target="_blank">github.com/AlLevykin</a></h5>
               </MDBCol>
             </MDBRow>
           </MDBCol>
@@ -196,6 +209,242 @@ class App extends Component {
             </MDBRow>
           </MDBCol>
         </MDBRow>
+        <MDBRow>
+          <h2 className="h1-responsive font-weight-bold white-text text-center mx-auto mb-5">          
+            Кейсы и решения
+          </h2>
+        </MDBRow> 
+        <hr style={{border: '1px solid white'}}></hr>
+        <MDBRow>
+          <MDBCol md="6">
+          <MDBRow className="mb-3">
+            <h3 className="font-weight-bold mb-3 white-text">Региональный этап 2019, Челябинск</h3>
+          </MDBRow>
+          <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="question-circle" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text">Кейс</h5>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <p className="white-text">Организовать сервис для сбора проектов на благоустройство территории. Проекты, получившие наибольшую поддержку от жильцов, направляются на повестку общего собрания или напрямую включаются в план работ организации, ответственной за обслуживание территории.</p>  
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="file-pdf" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={chel} rel="noopener noreferrer" target="_blank">Презентация</a></h5>
+              </MDBCol>
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="trophy" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={chel_diplom} rel="noopener noreferrer" target="_blank">Диплом</a></h5>
+              </MDBCol>
+            </MDBRow>       
+          </MDBCol>
+          <MDBCol md="6">
+          <MDBView rounded> 
+            <img src={chel_photo} className="img-fluid z-depth-3" alt="" />
+          </MDBView> 
+          </MDBCol>
+        </MDBRow>
+        <hr style={{border: '1px solid white'}}></hr>
+        <MDBRow>
+          <MDBCol md="6">
+          <MDBView rounded> 
+            <img src={kazan_photo} className="img-fluid z-depth-3" alt="" />
+          </MDBView>
+          </MDBCol>
+          <MDBCol md="6">
+          <MDBRow className="mb-3">
+            <h3 className="font-weight-bold mb-3 white-text">Финал 2019, Казань</h3>
+          </MDBRow>
+          <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="question-circle" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text">Кейс</h5>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <p className="white-text">Создать цифровую платформу на базе СПО для оказания «бумажных» и цифровых услуг. Оцифровка услуг силами аналитиков, без привлечения разработчиков: простая, интуитивно-понятная система создания форм заявления для их представления на Портале услуг, мобильном приложении, МФЦ, создание пользователей у органов власти, настройка для них прав обработки заявлений, создание логики обработки заявления, настройки сроков оказания и алгоритмов действий при просрочке и прочий функционал.</p>  
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="file-pdf" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={kazan} rel="noopener noreferrer" target="_blank">Презентация</a></h5>
+              </MDBCol>
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="trophy" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={kazan_diplom} rel="noopener noreferrer" target="_blank">Диплом</a></h5>
+              </MDBCol>
+            </MDBRow>        
+          </MDBCol>          
+        </MDBRow>  
+        <hr style={{border: '1px solid white'}}></hr>  
+        <MDBRow>
+          <MDBCol md="6">
+          <MDBRow className="mb-3">
+            <h3 className="font-weight-bold mb-3 white-text">ОНЛАЙН-ХАКАТОН #2, 2020</h3>
+          </MDBRow>
+          <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="question-circle" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text">Кейс</h5>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <p className="white-text">Сервис VKanban на платформе VK mini apps для управления проектами по методологии Kanban. Целевая аудитория: участники волонтерского движения, малый бизнес и любые сообщества и коллаборации, возникающие для выполнения общих проектов и социального партнерства. VKanban  систематизирует ведение проектов, позволяет решать организационные задачи, улучшает коммуникацию участников проектов, предоставляет возможность поиска партнеров и поставщиков услуг.</p>  
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="link" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href="https://hack2.leadersofdigital.ru/home.html#!/tab/198625950-3" rel="noopener noreferrer" target="_blank">Подробней о кейсе</a></h5>
+              </MDBCol>
+            </MDBRow>  
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="file-pdf" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={online} rel="noopener noreferrer" target="_blank">Презентация</a></h5>
+              </MDBCol>
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="trophy" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={online_diplom} rel="noopener noreferrer" target="_blank">Диплом</a></h5>
+              </MDBCol>
+            </MDBRow>       
+          </MDBCol>
+          <MDBCol md="6">
+            <div className="embed-responsive embed-responsive-16by9 z-depth-1-half">
+            <iframe title="online_demo" width="560" height="315" src="https://www.youtube.com/embed/PHc0_byNTR0?controls=0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </MDBCol>
+        </MDBRow>  
+        <hr style={{border: '1px solid white'}}></hr>
+        <MDBRow>
+          <MDBCol md="6">
+            <div className="embed-responsive embed-responsive-16by9 z-depth-1-half">
+            <iframe title="ekb_demo" width="560" height="315" src="https://www.youtube.com/embed/hEem3n_KQ34?controls=0" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div><br></br>
+            <div className="embed-responsive embed-responsive-16by9 z-depth-1-half">
+            <iframe title="ekb_presentation" width="560" height="315" src="https://www.youtube.com/embed/_eokUEOFT9Q?controls=0&amp;start=1911" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>            
+          </MDBCol>
+          <MDBCol md="6">
+          <MDBRow className="mb-3">
+            <h3 className="font-weight-bold mb-3 white-text">УРАЛЬСКИЙ IT-ХАБ 2020, Екатеринбург</h3>
+          </MDBRow>
+          <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="question-circle" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text">Кейс</h5>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <p className="white-text">На основании картографических данных об имеющихся на территории объектах инфраструктуры (кафе, магазины, банки и прочее) и данных о населении (с детализацией до здания) – требуется разработать алгоритм анализирующий спрос на тот или иной объект инфраструктуры, и предлагающий его оптимальное место размещения. В базовом сценарии требуется предложить варианты оптимального размещения Отделений почтовой связи, и подобрать оптимальный перечень услуг оказываемых в отделении.</p>  
+            </MDBRow>  
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="link" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href="https://leadersofdigital.ru/event/63009/case/178873" rel="noopener noreferrer" target="_blank">Подробней о кейсе</a></h5>
+              </MDBCol>
+            </MDBRow>  
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="file-pdf" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={ekb} rel="noopener noreferrer" target="_blank">Презентация</a></h5>
+              </MDBCol>
+            </MDBRow>   
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="trophy" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={ekb_diplom} rel="noopener noreferrer" target="_blank">Диплом</a></h5>
+              </MDBCol>
+            </MDBRow>        
+          </MDBCol>          
+        </MDBRow>
+        <hr style={{border: '1px solid white'}}></hr>  
+        <MDBRow>
+          <MDBCol md="6">
+          <MDBRow className="mb-3">
+            <h3 className="font-weight-bold mb-3 white-text">Финал, 2020</h3>
+          </MDBRow>
+          <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="question-circle" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text">Кейс</h5>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <p className="white-text">Одним из основных факторов, влияющих на качество оказываемой медицинской помощи, является квалификация врачей, а именно умение определять адекватную тактику диагностики, исследования, лечения пациента, а также прогнозирования неблагоприятных исходов. Оценка объективного статуса пациента целиком основана на признаках заболевания, как клинических, так и инструментально-лабораторных. В данном случае актуальное значение принимают так называемые интеллектуальные системы поддержки принятия врачебных решений, призванные обеспечить повышение качества медицинского обслуживания в учреждениях лечебно-профилактического профиля.</p>  
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="link" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href="https://leadersofdigital.ru/event/214763/case/239658" rel="noopener noreferrer" target="_blank">Подробней о кейсе</a></h5>
+              </MDBCol>
+            </MDBRow>    
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="file-pdf" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={final2020} rel="noopener noreferrer" target="_blank">Презентация</a></h5>
+              </MDBCol>
+            </MDBRow>                
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="trophy" size="2x" className="white-text" />
+              </MDBCol>
+              <MDBCol size="11">
+                <h5 className="font-weight-bold mb-3 white-text"><a className="white-text" href={final2020_diplom} rel="noopener noreferrer" target="_blank">Диплом</a></h5>
+              </MDBCol>
+            </MDBRow>       
+          </MDBCol>
+          <MDBCol md="6">
+          <MDBView rounded> 
+            <img src={final2020_photo} className="img-fluid z-depth-3" alt="" />
+          </MDBView> 
+          <br></br>
+            <div className="embed-responsive embed-responsive-16by9 z-depth-1-half">
+            <iframe title="final2020_presentation" width="560" height="315" src="https://www.youtube.com/embed/j0yvXy3IZEc?controls=0&amp;start=17150" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>              
+          </MDBCol>
+        </MDBRow>            
       </section>
       </MDBContainer>
     );
